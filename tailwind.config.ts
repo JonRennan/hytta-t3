4051636f4492716a996fad9fc2c7547200c23de4
jonren <jonren@stud.ntnu.no>
2024-06-30 17:58:46 +0200

    add color palette to tailwind and add .ts files to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,64 @@
-import {type Config} from "tailwindcss";
-import {fontFamily} from "tailwindcss/defaultTheme";
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-    content: ["./src/**/*.tsx"],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-            },
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          base: "var(--color-primary)",
+          on: "var(--color-on-primary)",
+          container: "var(--color-primary-container)",
+          container_on: "var(--color-on-primary-container)",
         },
+
+        secondary: {
+          base: "var(--color-secondary)",
+          on: "var(--color-on-secondary)",
+          container: "var(--color-secondary-container)",
+          container_on: "var(--color-on-secondary-container)",
+        },
+
+        tertiary: {
+          base: "var(--color-tertiary)",
+          on: "var(--color-on-tertiary)",
+          container: "var(--color-tertiary-container)",
+          container_on: "var(--color-on-tertiary-container)",
+        },
+
+        error: {
+          base: "var(--color-error)",
+          on: "var(--color-on-error)",
+          container: "var(--color-error-container)",
+          container_on: "var(--color-on-error-container)",
+        },
+
+        surface: {
+          dim: "var(--color-surface-dim)",
+          base: "var(--color-surface)",
+          bright: "var(--color-surface-bright)",
+          container_lowest: "var(--color-surface-container-lowest)",
+          container_low: "var(--color-surface-container-low)",
+          container: "var(--color-surface-container)",
+          container_high: "var(--color-surface-container-high)",
+          container_highest: "var(--color-surface-container-highest)",
+          on: "var(--color-on-surface)",
+          on_variant: "var(--color-on-surface-variant)",
+        },
+
+        outline: {
+          base: "var(--color-outline)",
+          variant: "var(--color-outline-variant)",
+        },
+
+        airbnb: "var(--color-airbnb)",
+        hotpink: "hotpink",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 } satisfies Config;
