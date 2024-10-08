e1ac5d35466780d79c0a8284806c9ed258012c6b
Jon Rennan <jonren@stud.ntnu.no>
2024-10-08 09:55:30 +0200

    make title and image in CabinCard links to calendar

--- src/components/cabin/cabin-card.tsx
+++ src/components/cabin/cabin-card.tsx
@@ -12,15 +12,21 @@ export default function CabinCard({ cabin }: CabinCardProps) {
   return (
     <div className="flex w-full max-w-sm flex-col items-center gap-1 rounded bg-surface-container p-4">
       {cabin.imageLink && (
-        <Image
-          src={cabin.imageLink}
-          width={256}
-          height={150}
-          alt={cabin.name ? cabin.name : "Hytta"}
-          className="rounded"
-        />
+        <Link href={`/kalender/${cabin.id}`}>
+          <Image
+            src={cabin.imageLink}
+            width={256}
+            height={150}
+            alt={cabin.name ? cabin.name : "Hytta"}
+            className="rounded"
+          />
+        </Link>
       )}
-      <h1 className="text-2xl font-bold uppercase">{cabin.name}</h1>
+      <Button variant="link">
+        <Link href={`/kalender/${cabin.id}`}>
+          <h1 className="text-2xl font-bold uppercase">{cabin.name}</h1>
+        </Link>{" "}
+      </Button>
       <p>{cabin.description}</p>
       {cabin.gmapsLink ? (
         <Button variant="link" className="max-w-64">
@@ -32,7 +38,7 @@ export default function CabinCard({ cabin }: CabinCardProps) {
         <p>{cabin.address}</p>
       )}
       <Button variant="link">
-        <Link href={`/kalender/${cabin.id}`}>Se kalender -{">"}</Link>{" "}
+        <Link href={`/kalender/${cabin.id}`}>Se kalender -{">"}</Link>
       </Button>
     </div>
   );
